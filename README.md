0106b29feafdcdec758bf045d6d353a4fe4697ee
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2021-07-24 20:11:39 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# poe-wiki-decrapifier
---
+++ background.js
@@ -0,0 +1,14 @@
+$(".global-navigation").remove()
+$(".mixed-content-footer").remove()
+$(".wds-global-footer").remove()
+$(".ad_unit").remove()
+$(".page__right-rail").remove()
+$(".mixed-content-footer").remove()
+
+$(".main-container").css('margin-left', "0px");
+$(".main-container").css("width", "100%");
+
+
+$(".resizable-container").css("margin", "20px");
+$(".resizable-container").css("max-width", "100%");
+$(".resizable-container").css("width", `${$(window).width()-40}px`);
